3fc3d6e91e4e98ee5357232b08a4d0e7933a3bcb
Nicolas Trinquier <nstq@protonmail.ch>
2020-01-12 00:39:44 +0100

    Updates

---
+++ precache-manifest.0a5fe3da4d150a4ebafee42ddacd6e1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5559840ed0ad5178e6c96a5a8e5a322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6e9b932958bbc7cf1bd",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "df643a30c413539b0e19",
+    "url": "/static/js/2.d4efd3dc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d4efd3dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6e9b932958bbc7cf1bd",
+    "url": "/static/js/main.e70bb29c.chunk.js"
+  },
+  {
+    "revision": "dc00896a146a0ceae044",
+    "url": "/static/js/runtime-main.a1a96cc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
